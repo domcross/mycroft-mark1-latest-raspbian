17a1e7a16849fce1c54be19ab449082da1c85722
Stuart Mumford <stuart@cadair.com>
2022-04-05 22:36:37 +0100

    fix card name

--- mark1-setup2.sh
+++ mark1-setup2.sh
@@ -1,14 +1,14 @@
 echo '# Setup soundcard - set correct volume and sound card settings'
 #Raise 'Master' volume to 46
-amixer -D hw:sndpriproto sset 'Master' 46%
+amixer -D hw:sndrpiproto sset 'Master' 46%
 #enable 'Master Playback'
 amixer -D hw:sndrpiproto sset 'Master Playback ZC' on
 #enable 'Mic' 'Capture' 
 amixer -D hw:sndrpiproto cset iface=MIXER,name='Mic Capture Switch' on
 #enable 'Playback Deemp'
-amixer -D hw:sndpriproto sset 'Playback Deemphasis' on
+amixer -D hw:sndrpiproto sset 'Playback Deemphasis' on
 #set 'Input Mux' to 'Mic'
-amixer -D hw:sndpriproto sset 'Input Mux' 'Mic'
+amixer -D hw:sndrpiproto sset 'Input Mux' 'Mic'
 #enable 'Output Mixer HiFi'
 amixer -D hw:sndrpiproto sset 'Output Mixer HiFi' on
 #enable 'Store DC Offset'
